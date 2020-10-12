6fb7298ebe8d0ca690595666647f2b2f2a6dec73
Suizhazha <573505403@qq.com>
2020-10-12 15:02:11 +0800

    update

---
+++ precache-manifest.3c0ea923a93c5bf0f843e191cc5c6744.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff8141f10ef4aa26b911744724fba9b",
+    "url": "/suixinjizhang-app-website/QRCode.png"
+  },
+  {
+    "revision": "33566aea4cdf429561d7",
+    "url": "/suixinjizhang-app-website/css/app.9d28d402.css"
+  },
+  {
+    "revision": "a1b1955af3f08533998a3a2f90a3a7dc",
+    "url": "/suixinjizhang-app-website/img/logo.a1b1955a.png"
+  },
+  {
+    "revision": "30db71cf3fa8267896870800fab5e800",
+    "url": "/suixinjizhang-app-website/index.html"
+  },
+  {
+    "revision": "33566aea4cdf429561d7",
+    "url": "/suixinjizhang-app-website/js/app.900b2fc8.js"
+  },
+  {
+    "revision": "c3da32766fca8b2a6e0b",
+    "url": "/suixinjizhang-app-website/js/chunk-vendors.d30a297a.js"
+  },
+  {
+    "revision": "578b8c6bccf94f0fda75c00d9bb123ed",
+    "url": "/suixinjizhang-app-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/suixinjizhang-app-website/robots.txt"
+  }
+]);
